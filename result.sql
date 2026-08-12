f0323dd8cf2cd9d7b33eec936d59dcb8c8a19088
Jalpan Bhavesh Mandavia <mandaviajb06@gmail.com>
2026-08-12 13:30:07 +0530

    Add result table with sample student data
    Created a table to store student results and inserted initial data.

---
+++ result.sql
@@ -0,0 +1,10 @@
+create table result(
+  student_roll_no int,
+  name varchar(20),
+  OE int,
+  SS int,
+  DAA int,
+  FOL int );
+insert into result values(1,'rohit',60,75,68,67), (2,'Manthan',70,87,98,57), (3,'ranveer',78,76,87,98),(4,'mohit',67,87,54,67);
+
+select*from marksheet;
